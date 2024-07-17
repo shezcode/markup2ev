4eed28ad3853e67239df1f2e52a2093a16f286c4
shez <shezcode@gmail.com>
2024-07-17 11:47:15 +0200

    basic stuff

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-async function fetchData() {
-  try {
-    const response = await fetch('http://localhost:3000/api/data');
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error(error);
+document.addEventListener('DOMContentLoaded', function () {
+  async function fetchData() {
+    try {
+      const response = await fetch('http://localhost:3000/api/data');
+      const data = await response.json();
+      console.log(data);
+      document.getElementById('title').textContent = data.title;
+      document.getElementById('intImg').src = `https://image.tmdb.org/t/p/original/${data.poster_path}`
+    } catch (error) {
+      console.error(error);
+    }
   }
-}
+  fetchData();
+})
 
-fetchData();
+
