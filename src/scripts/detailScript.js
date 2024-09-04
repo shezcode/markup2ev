cb62f4b06b7dc3cf6a267cc6362000bc926b2288
shez <shezcode@gmail.com>
2024-09-04 20:46:12 +0200

    final

--- src/scripts/detailScript.js
+++ src/scripts/detailScript.js
@@ -1,22 +1,44 @@
-async function fetchMovieData(id){
+async function fetchMovieData(id) {
   try {
     const response = await fetch(`http://localhost:3000/api/movieData/${id}`);
     const data = await response.json();
     console.log(data);
-    const movie = document.getElementById('movie-card');
-    const movieDiv = document.createElement('div');
-    movieDiv.classList.add('movie__card');
-    movieDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${data.poster_path})`
-    movieDiv.innerHTML = `
-      <h3>${data.original_title}</h3>
-      <h4>${data.release_date.split("-")[0]}</h4>
-      `;
-    movie.appendChild(movieDiv);
-  } catch (error){
-    console.error('oops');
+    const movie = document.getElementById("movie-card");
+    const titleDiv = document.createElement("div");
+    titleDiv.classList.add("movie__card--title");
+    titleDiv.innerHTML = `
+      <h1>${data.original_title} - ${data.release_date.split("-")[0]}</h1>
+    `;
+    movie.appendChild(titleDiv);
+    const parentDiv = document.createElement("div");
+    parentDiv.classList.add("movie__card--parent");
+    const movieDiv = document.createElement("div");
+    movieDiv.classList.add("movie__card-detail");
+    movieDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${data.poster_path})`;
+    parentDiv.appendChild(movieDiv);
+
+    let dataDiv = document.createElement("div");
+    dataDiv.classList.add("data__card");
+    dataDiv.innerHTML = `
+      <p><strong>Overview</strong>: ${data.overview}</p>
+      <p><strong>Runtime</strong>: ${data.runtime} minutes</p>
+      <p><strong>Genre</strong>: ${data.genres[0].name}</p>
+      <p><strong>Main actor: </strong>: ${data.cast[0].name}</p>
+    `;
+    if (data.homepage !== "") {
+      const link = document.createElement("a");
+      link.href = data.homepage;
+      link.target = "_blank";
+      link.textContent = "Visit Website";
+      dataDiv.appendChild(link);
+    }
+    parentDiv.appendChild(dataDiv);
+    movie.appendChild(parentDiv);
+  } catch (error) {
+    console.error("oops");
   }
 }
 
 const params = new URLSearchParams(document.location.search);
-const id = params.get('id');
-fetchMovieData(id);
+const id = params.get("id");
+window.onload = fetchMovieData(id);
--- src/scripts/search.js
+++ src/scripts/search.js
@@ -1,34 +1,40 @@
-function viewDetails(id){
-  window.location.href = `detail.html?id=${id}`
+function viewDetails(id) {
+  window.location.href = `detail.html?id=${id}`;
 }
 
-function displayData(data){
-  let movies = document.getElementById('movie-list');
-  movies.innerHTML = '';
-  data.forEach(movie => {
-    let movieDiv = document.createElement('div');
-    movieDiv.classList.add('movie__card');
-    movieDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${movie.poster_path})`
+function displayData(data) {
+  let movies = document.getElementById("movie-list");
+  movies.innerHTML = "";
+  data.forEach((movie) => {
+    let movieDiv = document.createElement("div");
+    movieDiv.classList.add("movie__card");
+    movieDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${movie.poster_path})`;
     movieDiv.innerHTML = `
-      <h3>${movie.original_title}</h3>
-      <h4>${movie.release_date.split("-")[0]}</h4>
-      <button class="movie__card--button" onclick="viewDetails(${movie.id})">View Details</button>
+      <div class="movie__banner">
+        <div class="movie__banner--extra">
+          <h3>${movie.original_title}</h3>
+          <h4>${movie.release_date.split("-")[0]}</h4>
+        </div>
+      <p>Rating: ${movie.vote_average}</p>
+      <button class="movie__card--button" onclick="viewDetails(${
+        movie.id
+      })">View Details</button>
+      </div>
       `;
     movies.appendChild(movieDiv);
-  })
+  });
 }
 
-document.getElementById("search-form").addEventListener('submit', (event) => {
+document.getElementById("search-form").addEventListener("submit", (event) => {
   event.preventDefault();
-  const query = document.querySelector('input').value;
+  const query = document.querySelector("input").value;
   searchItems(query);
-})
+});
 
-function searchItems(query){
-  const lowerCaseQuery = query.toLowerCase(); 
-  const filteredData = apiData.filter(item => 
+function searchItems(query) {
+  const lowerCaseQuery = query.toLowerCase();
+  const filteredData = apiData.filter((item) =>
     item.title.toLowerCase().includes(lowerCaseQuery)
   );
   displayData(filteredData);
 }
-
